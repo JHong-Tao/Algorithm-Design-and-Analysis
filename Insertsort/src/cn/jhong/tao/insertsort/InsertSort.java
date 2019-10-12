18ea8884a5d82bbce8d966ac9dcae308c04c62a4
JHong-Tao <1924775471@qq.com>
2019-10-12 19:44:31 +0800

    初始化插入排序项目

---
+++ Insertsort/src/cn/jhong/tao/insertsort/InsertSort.java
@@ -0,0 +1,11 @@
+package cn.jhong.tao.insertsort;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Author: JHong.Tao
+ * Date: 2019-10-12-19:41
+ * Version：1.0.0
+ * Description:
+ */
+public class InsertSort {
+}
